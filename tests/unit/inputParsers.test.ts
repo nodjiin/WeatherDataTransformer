b4e0e5ec4d31c5da486ab996e2ad79687f8bf142
alex casadio <alex.casadio92@gmail.com>
2023-09-17 13:56:52 +0800

    added unit tests for safeParseWeatherData

--- tests/unit/inputParsers.test.ts
+++ tests/unit/inputParsers.test.ts
@@ -1,4 +1,4 @@
-import { safeParseInputFilename } from '../../src/modules/io/inputParsers'
+import { safeParseInputFilename, safeParseWeatherData } from '../../src/modules/io/inputParsers'
 
 describe('inputParsers', () => {
     describe('safeParseInputFilename', () => {
@@ -30,6 +30,60 @@ describe('inputParsers', () => {
         it('should return full path for valid filenames', () => {
             const result = safeParseInputFilename('testfile.txt')
             expect(result).toContain('testfile.txt')
+        })
+    })
+
+    describe('safeParseWeatherData', () => {
+        it('should return parsed data for valid input', () => {
+            const validInput = `{
+                "cityName": {
+                    "name": "City Name",
+                    "lat": 1.0,
+                    "lon": 1.0,
+                    "timezone": "Timezone",
+                    "timezone_offset": 1,
+                    "current": {
+                        "dt": 1,
+                        "temp": 1,
+                        "feels_like": 1,
+                        "pressure": 1,
+                        "humidity": 1,
+                        "dew_point": 1,
+                        "uvi": 1,
+                        "clouds": 1,
+                        "visibility": 1,
+                        "wind_speed": 1,
+                        "wind_deg": 1,
+                        "weather": [{"id": 1, "main": "Main", "description": "Description", "icon": "Icon"}]
+                    },
+                    "hourly": [{
+                        "dt": 1,
+                        "temp": 1,
+                        "feels_like": 1,
+                        "pressure": 1,
+                        "humidity": 1,
+                        "dew_point": 1,
+                        "uvi": 1,
+                        "clouds": 1,
+                        "visibility": 1,
+                        "wind_speed": 1,
+                        "wind_deg": 1,
+                        "weather": [{"id": 1, "main": "Main", "description": "Description", "icon": "Icon"}]
+                    }]
+                }
+            }`
+
+            expect(safeParseWeatherData(validInput)).toBeTruthy()
+        })
+
+        it('should return null for invalid JSON input', () => {
+            const invalidJson = `{ "key": "value"`
+            expect(safeParseWeatherData(invalidJson)).toBeNull()
+        })
+
+        it('should return null for valid JSON but invalid weather data structure', () => {
+            const invalidWeatherData = `{"cityName": {"name": "City Name"}}`
+            expect(safeParseWeatherData(invalidWeatherData)).toBeNull()
         })
     })
 })
