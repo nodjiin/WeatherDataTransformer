ab4538c04d4ff9ad6585752fde14241c02cd28ef
alex casadio <alex.casadio92@gmail.com>
2023-09-22 18:18:55 +0800

    added documentation to errLoggers

--- src/modules/log/errLoggers.ts
+++ src/modules/log/errLoggers.ts
@@ -1,3 +1,10 @@
+/**
+ * Safely logs errors to the console. Handles both instances of `Error` and other error formats.
+ *
+ * @function
+ * @param {unknown} err - The error object or value to log.
+ * @param {string} baseMessage - The base message to prepend to the error message.
+ */
 export function safeLogError(err: unknown, baseMessage: string) {
     if (err instanceof Error) {
         console.error(`${baseMessage}: ${err.message}`)
